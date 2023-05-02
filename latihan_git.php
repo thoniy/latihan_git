1a06429015b261be187e5e02842d2480d5e4ca44
thoniy <132033061+thoniy@users.noreply.github.com>
2023-05-02 12:56:39 +0700

    latihan_git.php

---
+++ latihan_git.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>latihan_git</title>
+</head>
+<body>
+<?php
+echo "<h1>Data Diri</h1>";
+echo "<p>nama : Thoni Sugiyarto</p>";
+echo "<p>alamat : Trugo rt.07/rw.03, Jatiwarno, Jatipuro, Karanganyar.</p>";
+?>
+</body>
+</html>
